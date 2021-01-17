ad74eb7a178779af5d7fee01548d4c480a83ddd4
elif ozturk <elf.oztrk.66@gmail.com>
2021-01-17 18:52:19 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+
+        public void CustomerAdd(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + " " + "İsimli Muşteri Eklendi");
+        }
+
+        public void CustomerDelete(Musteri musteri)
+        {
+            if(musteri.Id==1)
+            {
+                Console.WriteLine(musteri.Ad + " " + "İsimli Muşteri Silindi");
+            }
+
+            else
+            {
+                Console.WriteLine(musteri.Ad + " " + "İsimli Muşteriyi Silemezsiniz");
+            }
+          
+        }
+
+        public void CustomerList(Musteri[] musteriler)
+        {
+            Console.WriteLine("Müşteri Listesi");
+            foreach (Musteri item in musteriler)
+            {
+                Console.WriteLine(item.Ad + "/ " + item.Soyad + "/" + item.MusteriNo);
+            }
+
+        }
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+
+            musteri1.Ad = "Ayşe";
+            musteri1.Soyad = "Yıldızzzz";
+            musteri1.Id = 1;
+            musteri1.MusteriNo = "123456";
+
+            Musteri musteri2 = new Musteri();
+
+            musteri2.Ad = "Fatma";
+            musteri2.Soyad = "sajdghj";
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "345678";
+
+            Musteri musteri3= new Musteri();
+
+            musteri3.Ad = "Elif";
+            musteri3.Soyad = "Öztürk";
+            musteri3.Id = 3;
+            musteri3.MusteriNo = "234567";
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+            MusteriManager musteriManager = new MusteriManager();
+
+            musteriManager.CustomerAdd(musteri1);
+            musteriManager.CustomerDelete(musteri2);
+            musteriManager.CustomerList(musteriler);
+
+
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
